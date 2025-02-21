29f51f02a02cfa8dd975debd9106821ec12a0b15
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-02-21 02:58:43 +0200

    add the edge detection window

--- view/edge_detection_window.py
+++ view/edge_detection_window.py
@@ -8,85 +8,82 @@ class EdgeDetectionsWindow(BasicStackedWindow):
     def __init__(self):
         super().__init__()
 
-        self.edge_detector_type_custom_combo_box = CustomComboBox(label= "Filter Type",combo_box_items_list=["Average Filter","Gaussian Filter","Median Filter","Low Pass Filter","High Pass Filter"])
-        self.edge_detector_type_custom_combo_box.currentIndexChanged.connect(self.on_filter_type_change)
+        self.edge_detector_type_custom_combo_box = CustomComboBox(label= "Edge Detector Type",combo_box_items_list=["Sobel Detector","Roberts Detector","Prewitt Detector","Canny Detector"])
+        self.edge_detector_type_custom_combo_box.currentIndexChanged.connect(self.on_edge_detector_type_change)
         self.controls_container_layout.addWidget(self.edge_detector_type_custom_combo_box)
 
 
-        self.average_filter_inputs_container = QWidget()
-        self.average_filter_inputs_container_layout = QHBoxLayout(self.average_filter_inputs_container)
-        self.average_filter_inputs_container_layout.setContentsMargins(0,0,0,0)
-        self.controls_container_layout.addWidget(self.average_filter_inputs_container)
-        self.average_filter_kernel_size_spin_box = CustomSpinBox(label="Kernel Size",range_start=3,range_end=9,initial_value=3,step_value=1)
-        self.average_filter_inputs_container_layout.addWidget(self.average_filter_kernel_size_spin_box)
+        self.sobel_detector_inputs_container = QWidget()
+        self.sobel_detector_inputs_container_layout = QHBoxLayout(self.sobel_detector_inputs_container)
+        self.sobel_detector_inputs_container_layout.setContentsMargins(0,0,0,0)
+        self.controls_container_layout.addWidget(self.sobel_detector_inputs_container)
+        self.sobel_detector_direction_custom_combo_box = CustomComboBox(label = "Direction",combo_box_items_list=["Horizontal","Vertical","Combined"])
+        self.sobel_detector_kernel_size_spin_box = CustomSpinBox(label="Kernel Size",range_start=3,range_end=9,initial_value=3,step_value=1)
+        self.sobel_detector_inputs_container_layout.addWidget(self.sobel_detector_direction_custom_combo_box)
+        self.sobel_detector_inputs_container_layout.addWidget(self.sobel_detector_kernel_size_spin_box)
 
 
-        self.gaussian_filter_inputs_container = QWidget()
-        self.gaussian_filter_inputs_container_layout = QHBoxLayout(self.gaussian_filter_inputs_container)
-        self.gaussian_filter_inputs_container_layout.setContentsMargins(0,0,0,0)
-        self.controls_container_layout.addWidget(self.gaussian_filter_inputs_container)
-        self.gaussian_filter_inputs_container.setVisible(False)
-        self.guassian_filter_kernel_size_spin_box = CustomSpinBox(label="Kernel Size",range_start=3,range_end=9,initial_value=3,step_value=1)
-        self.guassian_filter_variance_spin_box  = CustomSpinBox(label="Variance")
-        self.gaussian_filter_inputs_container_layout.addWidget(self.guassian_filter_kernel_size_spin_box)
-        self.gaussian_filter_inputs_container_layout.addWidget(self.guassian_filter_variance_spin_box)
+        self.roberts_detector_inputs_container = QWidget()
+        self.roberts_detector_inputs_container_layout = QHBoxLayout(self.roberts_detector_inputs_container)
+        self.roberts_detector_inputs_container_layout.setContentsMargins(0,0,0,0)
+        self.controls_container_layout.addWidget(self.roberts_detector_inputs_container)
+        self.roberts_detector_inputs_container.setVisible(False)
+        self.roberts_detector_kernel_size_spin_box = CustomSpinBox(label="Kernel Size",range_start=3,range_end=9,initial_value=3,step_value=1)
+        self.roberts_detector_inputs_container_layout.addWidget(self.roberts_detector_kernel_size_spin_box)
         
 
 
-        self.median_filter_inputs_container = QWidget()
-        self.median_filter_inputs_container_layout = QHBoxLayout(self.median_filter_inputs_container)
-        self.median_filter_inputs_container_layout.setContentsMargins(0,0,0,0)
-        self.controls_container_layout.addWidget(self.median_filter_inputs_container)
-        self.median_filter_inputs_container.setVisible(False)
-        self.median_filter_kernel_size_spin_box = CustomSpinBox(label="Kernel Size",range_start=3,range_end=9,initial_value=3,step_value=1)
-        self.median_filter_inputs_container_layout.addWidget(self.median_filter_kernel_size_spin_box)
+        self.prewitt_detector_inputs_container = QWidget()
+        self.prewitt_detector_inputs_container_layout = QHBoxLayout(self.prewitt_detector_inputs_container)
+        self.prewitt_detector_inputs_container_layout.setContentsMargins(0,0,0,0)
+        self.controls_container_layout.addWidget(self.prewitt_detector_inputs_container)
+        self.prewitt_detector_inputs_container.setVisible(False)
+        self.prewitt_detector_kernel_size_spin_box = CustomSpinBox(label="Kernel Size",range_start=3,range_end=9,initial_value=3,step_value=1)
+        self.prewitt_detector_inputs_container_layout.addWidget(self.prewitt_detector_kernel_size_spin_box)
 
 
-        self.low_pass_filter_inputs_container = QWidget()
-        self.low_pass_filter_inputs_container_layout = QHBoxLayout(self.low_pass_filter_inputs_container)
-        self.low_pass_filter_inputs_container_layout.setContentsMargins(0,0,0,0)
-        self.controls_container_layout.addWidget(self.low_pass_filter_inputs_container)
-        self.low_pass_filter_inputs_container.setVisible(False)
-        self.low_pass_filter_radius_spin_box = CustomSpinBox(label="Radius",range_start=3,range_end=9,initial_value=3,step_value=1)
-        self.low_pass_filter_inputs_container_layout.addWidget(self.low_pass_filter_radius_spin_box)
-
-        self.high_pass_filter_inputs_container = QWidget()
-        self.high_pass_filter_inputs_container_layout = QHBoxLayout(self.high_pass_filter_inputs_container)
-        self.high_pass_filter_inputs_container_layout.setContentsMargins(0,0,0,0)
-        self.controls_container_layout.addWidget(self.high_pass_filter_inputs_container)
-        self.high_pass_filter_inputs_container.setVisible(False)
-        self.high_pass_filter_radius_spin_box = CustomSpinBox(label="Radius",range_start=3,range_end=9,initial_value=3,step_value=1)
-        self.high_pass_filter_inputs_container_layout.addWidget(self.high_pass_filter_radius_spin_box)
+        self.canny_detector_inputs_container = QWidget()
+        self.canny_detector_inputs_container_layout = QHBoxLayout(self.canny_detector_inputs_container)
+        self.canny_detector_inputs_container_layout.setContentsMargins(0,0,0,0)
+        self.controls_container_layout.addWidget(self.canny_detector_inputs_container)
+        self.canny_detector_inputs_container.setVisible(False)
+        self.canny_detector_kernel_spin_box = CustomSpinBox(label="Kernel Size",range_start=3,range_end=9,initial_value=3,step_value=1)
+        self.canny_detector_variance_spin_box = CustomSpinBox(label="Variance",range_start=0,range_end=100,initial_value=0,step_value=1)
+        self.canny_detector_lower_threshold_spin_box = CustomSpinBox(label="Lower Threshold",range_start=0,range_end=100,initial_value=0,step_value=1)
+        self.canny_detector_upper_threshold_spin_box = CustomSpinBox(label="Upper Threshold",range_start=0,range_end=100,initial_value=0,step_value=1)
+        self.canny_detector_inputs_container_layout.addWidget(self.canny_detector_kernel_spin_box)
+        self.canny_detector_inputs_container_layout.addWidget(self.canny_detector_variance_spin_box)
+        self.canny_detector_inputs_container_layout.addWidget(self.canny_detector_lower_threshold_spin_box)
+        self.canny_detector_inputs_container_layout.addWidget(self.canny_detector_upper_threshold_spin_box)
 
 
         self.controls_container_layout.addStretch()
         
-        self.apply_filter_button = QPushButton("Apply")
-        self.controls_container_layout.addWidget(self.apply_filter_button)
+        self.apply_edge_Detector_button = QPushButton("Apply")
+        self.controls_container_layout.addWidget(self.apply_edge_Detector_button)
+
         
-    def on_filter_type_change(self):
+    def on_edge_detector_type_change(self):
         self.hide_all_inputs()
 
-        selected_filter = self.edge_detector_type_custom_combo_box.current_text()
+        selected_detector = self.edge_detector_type_custom_combo_box.current_text()
 
-        match selected_filter:
-            case "Average Filter":
-                self.average_filter_inputs_container.setVisible(True)
-            case "Gaussian Filter":
-                self.gaussian_filter_inputs_container.setVisible(True)
-            case "Median Filter":
-                self.median_filter_inputs_container.setVisible(True)
-            case "Low Pass Filter":
-                self.low_pass_filter_inputs_container.setVisible(True)
-            case "High Pass Filter":
-                self.high_pass_filter_inputs_container.setVisible(True)
-    
+        match selected_detector:
+            case "Sobel Detector":
+                self.sobel_detector_inputs_container.setVisible(True)
+            case "Roberts Detector":
+                self.roberts_detector_inputs_container.setVisible(True)
+            case "Prewitt Detector":
+                self.prewitt_detector_inputs_container.setVisible(True)
+            case "Canny Detector":
+                self.canny_detector_inputs_container.setVisible(True)
+
     def hide_all_inputs(self):
-        self.high_pass_filter_inputs_container.setVisible(False)
-        self.low_pass_filter_inputs_container.setVisible(False)
-        self.median_filter_inputs_container.setVisible(False)
-        self.gaussian_filter_inputs_container.setVisible(False)
-        self.average_filter_inputs_container.setVisible(False)
+        self.sobel_detector_inputs_container.setVisible(False)
+        self.roberts_detector_inputs_container.setVisible(False)
+        self.prewitt_detector_inputs_container.setVisible(False)
+        self.canny_detector_inputs_container.setVisible(False)
+
     
 
 
