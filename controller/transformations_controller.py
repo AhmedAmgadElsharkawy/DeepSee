22548285c5b3ea6f4de4d4df41fa338dc41c972f
somaiaahmed <somaia.ahmed03@gmail.com>
2025-02-25 23:29:53 +0200

    histogram

--- controller/transformations_controller.py
+++ controller/transformations_controller.py
@@ -1,3 +1,4 @@
+import numpy as np
 class TransformationsController():
     def __init__(self,transformations_window):
         self.transformations_window = transformations_window
@@ -5,3 +6,31 @@ class TransformationsController():
 
     def apply_transformation(self):
         print("applied")
+        
+    def grayScale_image(self, image):
+        return np.dot(image[...,:3], [0.2989, 0.5870, 0.1140]).astype(np.uint8)
+    
+    # compute grayscale histogram
+    def grayScale_histogram(self, image, min_range = 0, max_range = 256):
+        histogram , _ = np.histogram(image.flatten(), bins = 256, range = (min_range, max_range))
+        return histogram
+    
+    # compute RGB histogram 
+    def rgb_histogram(self, image, min_range = 0, max_range =256):
+        blue = image[:, :, 0]
+        green = image[:, :, 1]
+        red = image[:, :, 2]
+        
+        blue_histogram , _ = np.histogram(blue.flatten(), bins = 256, range = (min_range, max_range))
+        green_histogram , _ = np.histogram(green.flatten(), bins = 256, range = (min_range, max_range)) 
+        red_histogram , _ = np.histogram(red.flatten(), bins = 256, range = (min_range, max_range)) 
+        
+        return blue_histogram, green_histogram, red_histogram
+    
+    # compute grayscale cumulative histogram
+    def get_histogram(self, image, min_range=0, max_range=256):
+        if len(image.shape) == 2:  # Grayscale image
+            return self.grayScale_histogram(image, min_range, max_range)
+        elif len(image.shape) == 3:  # RGB image
+            return self.rgb_histogram(image, min_range, max_range)
+    
