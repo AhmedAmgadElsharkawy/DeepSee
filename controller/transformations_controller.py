d3c527f31c2162eaa0cd226f6bc19d79e92fc7f9
somaiaahmed <somaia.ahmed03@gmail.com>
2025-02-25 23:32:44 +0200

    cdf

--- controller/transformations_controller.py
+++ controller/transformations_controller.py
@@ -34,3 +34,27 @@ class TransformationsController():
         elif len(image.shape) == 3:  # RGB image
             return self.rgb_histogram(image, min_range, max_range)
     
+    def grayScale_cdf(self, image, min_range=0, max_range=256):
+        histogram = self.grayScale_histogram(image, min_range, max_range)
+        cdf = histogram.cumsum()
+        cdf_normalized = cdf * float(histogram.max()) / cdf.max()
+        return cdf_normalized
+    
+    def rgb_cdf(self, image, min_range=0, max_range=256):
+        blue_histogram, green_histogram, red_histogram = self.rgb_histogram(image, min_range, max_range)
+        
+        blue_cdf = blue_histogram.cumsum()
+        green_cdf = green_histogram.cumsum()
+        red_cdf = red_histogram.cumsum()
+        
+        blue_cdf_normalized = blue_cdf * float(blue_histogram.max()) / blue_cdf.max()
+        green_cdf_normalized = green_cdf * float(green_histogram.max()) / green_cdf.max()
+        red_cdf_normalized = red_cdf * float(red_histogram.max()) / red_cdf.max()
+        
+        return blue_cdf_normalized, green_cdf_normalized, red_cdf_normalized
+    
+    def get_cdf(self, image, min_range=0, max_range=256):
+        if len(image.shape) == 2:
+            return self.grayScale_cdf(image, min_range, max_range)  
+        elif len(image.shape) == 3:
+            return self.rgb_cdf(image, min_range, max_range)
