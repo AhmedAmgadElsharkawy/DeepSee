76b52c3287a0f2a5bf963dc585a2659fb4af9b41
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2025-02-26 22:11:09 +0200

    adding the uniform noise

--- controller/noise_controller.py
+++ controller/noise_controller.py
@@ -10,6 +10,8 @@ class NoiseController():
         gray_image = self.noise_window.input_image_viewer.image_model.get_gray_image_matrix()
         if type == "Gaussian Noise":
             result = self.add_gaussian_noise(gray_image)
+        elif type == "Uniform Noise":
+            result = self.add_uniform_noise(gray_image)
         self.noise_window.output_image_viewer.display_and_set_image_matrix(result)
 
     def add_gaussian_noise(self, image):
@@ -20,3 +22,9 @@ class NoiseController():
         noisy_image = np.clip(noisy_image, 0, 255)  # Clip values to valid range
         return noisy_image.astype(np.uint8)
     
+    def add_uniform_noise(self, image):
+        noise_level = self.noise_window.noise_value_spin_box.value()
+        noise = np.random.uniform(-noise_level, noise_level, image.shape).astype(np.float32)
+        noisy_image = image.astype(np.float32) + noise  # Add noise to image
+        noisy_image = np.clip(noisy_image, 0, 255)  # Ensure valid pixel range
+        return noisy_image.astype(np.uint8)
