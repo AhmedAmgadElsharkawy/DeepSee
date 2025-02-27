129d76927117ff26a049c0539fa0bc016148f7d1
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2025-02-27 16:33:31 +0200

    adding the global mean thresholding method

--- controller/thresholding_controller.py
+++ controller/thresholding_controller.py
@@ -4,15 +4,20 @@ class ThresholdingController():
         self.thresholding_window = thresholding_window
         self.thresholding_window.apply_button.clicked.connect(self.apply_thresholding)
 
+
     def apply_thresholding(self):
+        filter_type = self.thresholding_window.thresholding_type_custom_combo_box.current_text()
         gray_image = self.thresholding_window.input_image_viewer.image_model.get_gray_image_matrix()
-        thresholded_image = self.global_otsu(gray_image)
+        if filter_type == "Otsu Thresholding":
+            thresholded_image = self.global_otsu(gray_image)
+        elif filter_type == "Global Mean":
+            thresholded_image = self.global_mean(gray_image)
         self.thresholding_window.output_image_viewer.display_and_set_image_matrix(thresholded_image)
 
     def global_otsu(self, image):
         hist, bin_edges = np.histogram(image.ravel(), bins=256, range=[0, 256])
         total_pixels = image.size
-        hist = hist.astype(np.float32) / total_pixels  # Normalize
+        hist = hist.astype(np.float32) / total_pixels
         
         cumulative_sum = np.cumsum(hist)
         cumulative_mean = np.cumsum(hist * np.arange(256))
@@ -23,3 +28,16 @@ class ThresholdingController():
 
         thresholded_image = (image > optimal_threshold).astype(np.uint8) * 255
         return thresholded_image
+    
+    def global_mean(self, image):
+        T = np.mean(image)
+        thresholded = np.zeros_like(image)
+
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                if image[i, j] >= T:
+                    thresholded[i, j] = 255
+                else:
+                    thresholded[i, j] = 0
+
+        return thresholded
--- view/window/thresholding_window.py
+++ view/window/thresholding_window.py
@@ -10,7 +10,7 @@ class ThresholdingWindow(BasicStackedWindow):
     def __init__(self, main_window):
         super().__init__(main_window, "Thresholding")
 
-        self.thresholding_type_custom_combo_box = CustomComboBox(label= "Thresholding Type",combo_box_items_list=["Optimal Thresholding","Otsu Thresholding","Spectral Thresholding"])
+        self.thresholding_type_custom_combo_box = CustomComboBox(label= "Thresholding Type",combo_box_items_list=["Optimal Thresholding","Otsu Thresholding","Spectral Thresholding", "Global Mean"])
         self.inputs_container_layout.addWidget(self.thresholding_type_custom_combo_box)
 
         self.thresholding_scope_custom_combo_box = CustomComboBox(label= "Thresholding Scope",combo_box_items_list=["Global Thresholding","Local Thresholding"])
