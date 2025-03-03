b19de6573eaaf116b00f2358c38d444962a2a4e2
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2025-03-03 20:54:24 +0200

    making noise affect only colored images

--- controller/noise_controller.py
+++ controller/noise_controller.py
@@ -7,13 +7,12 @@ class NoiseController():
     def apply_noise(self):
         type = self.noise_window.noise_type_custom_combo_box.current_text()
         image = self.noise_window.input_image_viewer.image_model.get_image_matrix()
-        gray_image = self.noise_window.input_image_viewer.image_model.get_gray_image_matrix()
         if type == "Gaussian Noise":
-            result = self.add_gaussian_noise(gray_image)
+            result = self.add_gaussian_noise(image)
         elif type == "Uniform Noise":
-            result = self.add_uniform_noise(gray_image)
+            result = self.add_uniform_noise(image)
         else:
-            result = self.add_salt_and_pepper_noise(gray_image)
+            result = self.add_salt_and_pepper_noise(image)
         self.noise_window.output_image_viewer.display_and_set_image_matrix(result)
 
     def add_gaussian_noise(self, image):
