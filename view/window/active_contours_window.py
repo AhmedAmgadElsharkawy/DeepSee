769d22ebd303d48ec78069276133077dc079c857
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-03-25 02:34:14 +0200

    update the active contours window

--- view/window/active_contours_window.py
+++ view/window/active_contours_window.py
@@ -36,20 +36,50 @@ class ActiveContoursWindow(BasicStackedWindow):
         self.active_contours_detector_beta_spin_box = CustomSpinBox(label="Beta",range_start=0,range_end=100,initial_value=0,step_value=0.1,double_value=True,decimals=1)
         self.active_contours_detector_gamma_spin_box = CustomSpinBox(label="Gamma",range_start=0,range_end=100,initial_value=0,step_value=0.1,double_value=True,decimals=1)
         # ///////////////
+
+        self.statistics_container = QWidget()
+        self.statistics_container_layout = QHBoxLayout(self.statistics_container)
+        self.statistics_container_layout.setContentsMargins(0,0,0,0)
+
+
+        self.active_contours_detector_perimeter_container = QWidget()
+        self.statistics_container_layout.addWidget(self.active_contours_detector_perimeter_container)
+        self.active_contours_detector_perimeter_container_layout = QHBoxLayout(self.active_contours_detector_perimeter_container)
+        self.active_contours_detector_perimeter_container_layout.setContentsMargins(0,0,0,0)
         self.active_contours_detector_perimeter_label = QLabel("Contour Perimeter:")
         self.active_contours_detector_perimeter = QLineEdit()
         self.active_contours_detector_perimeter.setReadOnly(True)  # Prevent user editing
         self.active_contours_detector_perimeter.setText(f"{0.00:.2f} ")  # Default value
+        self.active_contours_detector_perimeter_container_layout.addWidget(self.active_contours_detector_perimeter_label)
+        self.active_contours_detector_perimeter_container_layout.addWidget(self.active_contours_detector_perimeter)
+
+        self.active_contours_detector_area_container = QWidget()
+        self.statistics_container_layout.addWidget(self.active_contours_detector_area_container)
+        self.active_contours_detector_area_container_layout = QHBoxLayout(self.active_contours_detector_area_container)
+        self.active_contours_detector_area_container_layout.setContentsMargins(0,0,0,0)
+
         self.active_contours_detector_area_label = QLabel("Contour Area:")
         self.active_contours_detector_area = QLineEdit()
         self.active_contours_detector_area.setReadOnly(True)  # Prevent user editing
         self.active_contours_detector_area.setText(f"{0.00:.2f} ")  # Default value
+        self.active_contours_detector_area_container_layout.addWidget(self.active_contours_detector_area_label)
+        self.active_contours_detector_area_container_layout.addWidget(self.active_contours_detector_area)
+        
+
+        self.active_contours_detector_chaincode_container = QWidget()
+        self.statistics_container_layout.addWidget(self.active_contours_detector_chaincode_container)
+        self.active_contours_detector_chaincode_container_layout = QHBoxLayout(self.active_contours_detector_chaincode_container)
+        self.active_contours_detector_chaincode_container_layout.setContentsMargins(0,0,0,0)
+
+
         self.active_contours_detector_chaincode_label = QLabel("Contour Chain Code:")
         self.active_contours_detector_chaincode = QLabel()
         self.active_contours_detector_chaincode.setTextInteractionFlags(Qt.TextSelectableByMouse)
         self.active_contours_detector_chaincode.setWordWrap(True)
         self.active_contours_detector_chaincode.setText("00000000000000000000000000000000")
         self.active_contours_detector_chaincode.setStyleSheet("font-family: Courier; font-size: 12px;")
+        self.active_contours_detector_chaincode_container_layout.addWidget(self.active_contours_detector_chaincode_label)
+        self.active_contours_detector_chaincode_container_layout.addWidget(self.active_contours_detector_chaincode)
 
         # self.active_contours_detector_chaincode = QLineEdit()
         # self.active_contours_detector_chaincode.setReadOnly(True)  # Prevent user editing
@@ -79,12 +109,14 @@ class ActiveContoursWindow(BasicStackedWindow):
         self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_alpha_spin_box)
         self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_beta_spin_box)
         self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_gamma_spin_box)
-        self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_perimeter_label)
-        self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_perimeter)
-        self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_area_label)
-        self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_area)
-        self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_chaincode_label)
-        self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_chaincode)
+        # self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_perimeter_label)
+        # self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_perimeter)
+        # self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_area_label)
+        # self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_area)
+        # self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_chaincode_label)
+        # self.active_contours_inputs_container_layout.addWidget(self.active_contours_detector_chaincode)
+
+        self.main_widget_layout.addWidget(self.statistics_container)
 
         self.active_contours_controller = ActiveContoursController(self)
         
