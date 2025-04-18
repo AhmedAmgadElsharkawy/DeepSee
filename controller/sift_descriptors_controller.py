0e92182528fc2087f7b062feaca8f8bde4a604a6
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2025-04-18 02:21:03 +0200

    test

--- controller/sift_descriptors_controller.py
+++ controller/sift_descriptors_controller.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from math import floor
+
 class SiftDescriptorsController():
     def __init__(self,sift_descriptors_window):
         self.sift_descriptors_window = sift_descriptors_window
@@ -10,8 +12,10 @@ class SiftDescriptorsController():
         image=self.sift_descriptors_window.detect_keypoints_input_image_viewer.image_model.get_gray_image_matrix()
 
         gaussian_pyramid=self.generate_gaussian_pyramid(image)
-        dog_pyramid=self.compute_dog_pyramid(gaussian_pyramid)
-        keypoints=self.detect_keypoints(dog_pyramid) #keypoint information (octave, scale, and coordinates) to the keypoints list.
+        dog_pyramid=self.generateDoGImages(gaussian_pyramid)
+        print(dog_pyramid)
+        keypoints=self.findScaleSpaceExtrema(gaussian_pyramid, dog_pyramid) #keypoint information (octave, scale, and coordinates) to the keypoints list.
+        print("KeyPoints: ", keypoints)
         self.sift_descriptors_window.detect_keypoints_output_image_viewer.display_and_set_image_matrix(dog_pyramid[0][1])
 
         print("sift")
@@ -48,6 +52,18 @@ class SiftDescriptorsController():
 
         return dog_pyramid
 
+    def generateDoGImages(self, gaussian_images):
+        """Generate Difference-of-Gaussians image pyramid
+        """
+        dog_images = []
+
+        for gaussian_images_in_octave in gaussian_images:
+            dog_images_in_octave = []
+            for first_image, second_image in zip(gaussian_images_in_octave, gaussian_images_in_octave[1:]):
+                dog_images_in_octave.append(np.subtract(second_image, first_image))  # ordinary subtraction will not work because the images are unsigned integers
+            dog_images.append(dog_images_in_octave)
+        return dog_images
+
     def detect_keypoints(self,dog_pyramid):
         """Detects keypoints by finding local extrema in the DoG pyramid."""
         keypoints = []
@@ -71,3 +87,114 @@ class SiftDescriptorsController():
 
         return keypoints
 
+    def findScaleSpaceExtrema(self, gaussian_images, dog_images, num_intervals=1, sigma=1.6, image_border_width=1, contrast_threshold=0.04):
+        """Find pixel positions of all scale-space extrema in the image pyramid
+        """
+        threshold = floor(0.5 * contrast_threshold / num_intervals * 255)  # from OpenCV implementation
+        keypoints = []
+        for octave_index, dog_images_in_octave in enumerate(dog_images):
+            for image_index, (first_image, second_image, third_image) in enumerate(zip(dog_images_in_octave, dog_images_in_octave[1:], dog_images_in_octave[2:])):
+                # (i, j) is the center of the 3x3 array
+                for i in range(1, second_image.shape[0] - 1):
+                    for j in range(1, second_image.shape[1] - 1):
+                        if self.isPixelAnExtremum(first_image[i-1:i+2, j-1:j+2], second_image[i-1:i+2, j-1:j+2], third_image[i-1:i+2, j-1:j+2], threshold):
+                            localization_result = self.localizeExtremumViaQuadraticFit(i, j, image_index + 1, octave_index, num_intervals, dog_images_in_octave, sigma, contrast_threshold, image_border_width)
+                            if localization_result is not None:
+                                keypoint, localized_image_index = localization_result
+                                keypoints_with_orientations = computeKeypointsWithOrientations(keypoint, octave_index, gaussian_images[octave_index][localized_image_index])
+                                for keypoint_with_orientation in keypoints_with_orientations:
+                                    keypoints.append(keypoint_with_orientation)
+        return keypoints
+
+    def isPixelAnExtremum(self, first_subimage, second_subimage, third_subimage, threshold):
+        """Return True if the center element of the 3x3x3 input array is strictly greater than or less than all its neighbors, False otherwise
+        """
+        center_pixel_value = second_subimage[1, 1]
+        if abs(center_pixel_value) > threshold:
+            if center_pixel_value > 0:
+                return np.all(center_pixel_value >= first_subimage) and \
+                    np.all(center_pixel_value >= third_subimage) and \
+                    np.all(center_pixel_value >= second_subimage[0, :]) and \
+                    np.all(center_pixel_value >= second_subimage[2, :]) and \
+                    center_pixel_value >= second_subimage[1, 0] and \
+                    center_pixel_value >= second_subimage[1, 2]
+            elif center_pixel_value < 0:
+                return np.all(center_pixel_value <= first_subimage) and \
+                    np.all(center_pixel_value <= third_subimage) and \
+                    np.all(center_pixel_value <= second_subimage[0, :]) and \
+                    np.all(center_pixel_value <= second_subimage[2, :]) and \
+                    center_pixel_value <= second_subimage[1, 0] and \
+                    center_pixel_value <= second_subimage[1, 2]
+        return False
+
+    def localizeExtremumViaQuadraticFit(self, i, j, image_index, octave_index, num_intervals, dog_images_in_octave, sigma, contrast_threshold, image_border_width, eigenvalue_ratio=10, num_attempts_until_convergence=5):
+        """Iteratively refine pixel positions of scale-space extrema via quadratic fit around each extremum's neighbors
+        """
+        extremum_is_outside_image = False
+        image_shape = dog_images_in_octave[0].shape
+        for attempt_index in range(num_attempts_until_convergence):
+            # need to convert from uint8 to float32 to compute derivatives and need to rescale pixel values to [0, 1] to apply Lowe's thresholds
+            first_image, second_image, third_image = dog_images_in_octave[image_index-1:image_index+2]
+            pixel_cube = np.stack([first_image[i-1:i+2, j-1:j+2],
+                                second_image[i-1:i+2, j-1:j+2],
+                                third_image[i-1:i+2, j-1:j+2]]).astype('float32') / 255.
+            gradient = self.computeGradientAtCenterPixel(pixel_cube)
+            hessian = self.computeHessianAtCenterPixel(pixel_cube)
+            extremum_update = -np.linalg.lstsq(hessian, gradient, rcond=None)[0]
+            if abs(extremum_update[0]) < 0.5 and abs(extremum_update[1]) < 0.5 and abs(extremum_update[2]) < 0.5:
+                break
+            j += int(round(extremum_update[0]))
+            i += int(round(extremum_update[1]))
+            image_index += int(round(extremum_update[2]))
+            # make sure the new pixel_cube will lie entirely within the image
+            if i < image_border_width or i >= image_shape[0] - image_border_width or j < image_border_width or j >= image_shape[1] - image_border_width or image_index < 1 or image_index > num_intervals:
+                extremum_is_outside_image = True
+                break
+        if extremum_is_outside_image:
+            return None
+        if attempt_index >= num_attempts_until_convergence - 1:
+            return None
+        functionValueAtUpdatedExtremum = pixel_cube[1, 1, 1] + 0.5 * np.dot(gradient, extremum_update)
+        if abs(functionValueAtUpdatedExtremum) * num_intervals >= contrast_threshold:
+            xy_hessian = hessian[:2, :2]
+            xy_hessian_trace = trace(xy_hessian)
+            xy_hessian_det = det(xy_hessian)
+            if xy_hessian_det > 0 and eigenvalue_ratio * (xy_hessian_trace ** 2) < ((eigenvalue_ratio + 1) ** 2) * xy_hessian_det:
+                # Contrast check passed -- construct and return OpenCV KeyPoint object
+                keypoint = KeyPoint()
+                keypoint.pt = ((j + extremum_update[0]) * (2 ** octave_index), (i + extremum_update[1]) * (2 ** octave_index))
+                keypoint.octave = octave_index + image_index * (2 ** 8) + int(round((extremum_update[2] + 0.5) * 255)) * (2 ** 16)
+                keypoint.size = sigma * (2 ** ((image_index + extremum_update[2]) / float32(num_intervals))) * (2 ** (octave_index + 1))  # octave_index + 1 because the input image was doubled
+                keypoint.response = abs(functionValueAtUpdatedExtremum)
+                return keypoint, image_index
+        return None
+
+    def computeGradientAtCenterPixel(self, pixel_array):
+        """Approximate gradient at center pixel [1, 1, 1] of 3x3x3 array using central difference formula of order O(h^2), where h is the step size
+        """
+        # With step size h, the central difference formula of order O(h^2) for f'(x) is (f(x + h) - f(x - h)) / (2 * h)
+        # Here h = 1, so the formula simplifies to f'(x) = (f(x + 1) - f(x - 1)) / 2
+        # NOTE: x corresponds to second array axis, y corresponds to first array axis, and s (scale) corresponds to third array axis
+        dx = 0.5 * (pixel_array[1, 1, 2] - pixel_array[1, 1, 0])
+        dy = 0.5 * (pixel_array[1, 2, 1] - pixel_array[1, 0, 1])
+        ds = 0.5 * (pixel_array[2, 1, 1] - pixel_array[0, 1, 1])
+        return [dx, dy, ds]
+
+    def computeHessianAtCenterPixel(self, pixel_array):
+        """Approximate Hessian at center pixel [1, 1, 1] of 3x3x3 array using central difference formula of order O(h^2), where h is the step size
+        """
+        # With step size h, the central difference formula of order O(h^2) for f''(x) is (f(x + h) - 2 * f(x) + f(x - h)) / (h ^ 2)
+        # Here h = 1, so the formula simplifies to f''(x) = f(x + 1) - 2 * f(x) + f(x - 1)
+        # With step size h, the central difference formula of order O(h^2) for (d^2) f(x, y) / (dx dy) = (f(x + h, y + h) - f(x + h, y - h) - f(x - h, y + h) + f(x - h, y - h)) / (4 * h ^ 2)
+        # Here h = 1, so the formula simplifies to (d^2) f(x, y) / (dx dy) = (f(x + 1, y + 1) - f(x + 1, y - 1) - f(x - 1, y + 1) + f(x - 1, y - 1)) / 4
+        # NOTE: x corresponds to second array axis, y corresponds to first array axis, and s (scale) corresponds to third array axis
+        center_pixel_value = pixel_array[1, 1, 1]
+        dxx = pixel_array[1, 1, 2] - 2 * center_pixel_value + pixel_array[1, 1, 0]
+        dyy = pixel_array[1, 2, 1] - 2 * center_pixel_value + pixel_array[1, 0, 1]
+        dss = pixel_array[2, 1, 1] - 2 * center_pixel_value + pixel_array[0, 1, 1]
+        dxy = 0.25 * (pixel_array[1, 2, 2] - pixel_array[1, 2, 0] - pixel_array[1, 0, 2] + pixel_array[1, 0, 0])
+        dxs = 0.25 * (pixel_array[2, 1, 2] - pixel_array[2, 1, 0] - pixel_array[0, 1, 2] + pixel_array[0, 1, 0])
+        dys = 0.25 * (pixel_array[2, 2, 1] - pixel_array[2, 0, 1] - pixel_array[0, 2, 1] + pixel_array[0, 0, 1])
+        return [[dxx, dxy, dxs], 
+                    [dxy, dyy, dys],
+                    [dxs, dys, dss]]
