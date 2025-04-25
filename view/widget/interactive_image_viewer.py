c2d6f43e81159ae3c10fdf92a0887532958f56ea
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2025-04-25 18:38:36 +0300

    add reset markers

--- view/widget/interactive_image_viewer.py
+++ view/widget/interactive_image_viewer.py
@@ -7,12 +7,11 @@ from view.widget.image_viewer import ImageViewer
 
 
 class InteractiveImageViewer(ImageViewer):
-    def __init__(self,custom_placeholder = None, add_markers = True):
-        super().__init__(type = "input",custom_placeholder = custom_placeholder)
+    def __init__(self, custom_placeholder=None):
+        super().__init__(type="input", custom_placeholder=custom_placeholder)
         self.setAcceptDrops(True)
-        if add_markers:
-            self.markers_positions = [] 
-            self.getView().scene().sigMouseClicked.connect(self.handle_mouse_click)
+        self.markers_positions = []
+        self.marker_items = []
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
@@ -30,9 +29,9 @@ class InteractiveImageViewer(ImageViewer):
             self.load_image(file_path)
 
     def load_image(self, file_path):
+        self.reset()
         self.image_model.load_image(file_path=file_path)
-        self.display_image_matrix(self.image_model.get_image_matrix())    
-
+        self.display_image_matrix(self.image_model.get_image_matrix())
 
     def handle_mouse_click(self, event):
         if event.button() == Qt.LeftButton and self.image_model.image_matrix is not None:
@@ -44,7 +43,6 @@ class InteractiveImageViewer(ImageViewer):
 
             if 0 <= x < width and 0 <= y < height:
                 self.add_x_marker(x, y)
-
 
     def add_x_marker(self, x, y, size=6):
         line1 = QGraphicsLineItem(x - size/2, y - size/2, x + size/2, y + size/2)
@@ -58,5 +56,24 @@ class InteractiveImageViewer(ImageViewer):
         self.getView().addItem(line1)
         self.getView().addItem(line2)
 
-        self.markers_positions.append({int(x), int(y)})
+        self.marker_items.extend([line1, line2])
+        self.markers_positions.append({"x" : int(x), 'y' : int(y)})
 
+    def reset_markers(self):
+        for item in self.marker_items:
+            self.getView().removeItem(item)
+        self.marker_items.clear()
+        self.markers_positions.clear()
+
+
+    def reset(self):
+        super().reset()  
+        self.reset_markers()
+
+
+    def enable_add_marker(self, enabled: bool):
+        if enabled:
+            self.getView().scene().sigMouseClicked.connect(self.handle_mouse_click)
+        elif not enabled:
+            self.getView().scene().sigMouseClicked.disconnect(self.handle_mouse_click)
+            self.reset_markers()
